4916251d716d70e86b65994f02fefbaf88189e4e
Ashehal Sadik <ashehalsadik@192.168.1.13>
2024-08-03 13:41:49 +0500

    A simple sign-up page.

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -1,41 +1,40 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import { Container } from 'react-bootstrap'
-import Row from 'react-bootstrap'
-import Col from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import './GetStarted.css'
 import { Link } from 'react-router-dom'
 
 const GetStarted = () => {
   return (
-    <div className='complete-screen-container'>
-        <div></div>
-        <div>
-            <div className='container-sm title'>
-                <h1>Learn With AI</h1>
-            </div>
-            <div className='container-sm'>
-                <Form>
-                    <Form.Group className='mb-3' controlId='sign-up-email'>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type='email'/>
-                    </Form.Group>
-
-                    <Form.Group className='mb-3' controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' />
-                    </Form.Group>
-                    <div className="d-grid"><Button variant='primary' type='submit' size='sm'>Get Started!</Button></div>
-                </Form>
-            </div>
-            <Container>
+    <div className="full-screen background">
+        <Container className='d-flex align-items-center justify-content-center fluid full-screen'>
+            <Col xs={4}>
+                <Container>
+                    <h1 className='text-center text-light'>Learn With AI</h1>
+                </Container>
+                <Container>
+                    <Form>
+                        <Form.Group className='mb-3' controlId='sign-up-email'>
+                            <Form.Label className='text-light'>Email address</Form.Label>
+                            <Form.Control type='email'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='password'>
+                            <Form.Label className='text-light'>Password</Form.Label>
+                            <Form.Control type='password' />
+                        </Form.Group>
+                        <Container className="d-grid m-0 p-0"><Button variant='primary' type='submit' size='sm'>Get Started!</Button></Container>
+                    </Form>
+                </Container>
+                <Container>
                 <Row>
-                    <Col>Already have an account? <Link to="/login">Sign in.</Link></Col>
+                    <Col className='text-center m-2 text-light'>Already have an account? <Link to="/login">Sign in.</Link></Col>
                 </Row>
-            </Container>
-        </div>
-        <div></div>
+                </Container>
+            </Col>
+        </Container>
     </div>
   )
 }
