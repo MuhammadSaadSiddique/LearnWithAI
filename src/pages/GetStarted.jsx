d8f1f6116e16e446ab44534618e3cbe79e51d256
Ashehal Sadik <ashehalsadik@192.168.1.13>
2024-08-03 12:10:01 +0500

    Trying to replace bootstrap with React Bootstrap.

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Container } from 'react-bootstrap'
+import Row from 'react-bootstrap'
+import Col from 'react-bootstrap'
+import './GetStarted.css'
+import { Link } from 'react-router-dom'
 
 const GetStarted = () => {
   return (
     <div className='complete-screen-container'>
+        <div></div>
         <div>
-            <div className='title'></div>
-            <div className='form'>
-                <form action="" method="post">
-                    <div className='form-group'>
-                        <label htmlFor="email-address">Email address</label>
-                        <input type="email" className='form-control' id='email-address' name='email-address'/>
-                    </div>
-
-                    <div className='form-group'>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className='form-control' id='password' name='password'/>
-                    </div>
-                    <button type='submit' className='btn btn-primary'>Submit</button>
-                </form>
+            <div className='container-sm title'>
+                <h1>Learn With AI</h1>
             </div>
-            <div className='login'></div>
+            <div className='container-sm'>
+                <Form>
+                    <Form.Group className='mb-3' controlId='sign-up-email'>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type='email'/>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' />
+                    </Form.Group>
+                    <div className="d-grid"><Button variant='primary' type='submit' size='sm'>Get Started!</Button></div>
+                </Form>
+            </div>
+            <Container>
+                <Row>
+                    <Col>Already have an account? <Link to="/login">Sign in.</Link></Col>
+                </Row>
+            </Container>
         </div>
+        <div></div>
     </div>
   )
 }
