7a3c4ea582de384c2cd005197c540c674c2ed9f3
Ashehal Sadik <ashehalomarsadik@gmail.com>
2024-08-06 16:00:42 +0500

    Resolve merge conflicts with main.

--- frontend/src/pages/Email.jsx
+++ frontend/src/pages/Email.jsx
@@ -1,10 +1,6 @@
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
-<<<<<<< HEAD
-
-const Email = () => {
-=======
 import { useNavigate } from 'react-router-dom'
 
 const Email = () => {
@@ -14,7 +10,6 @@ const Email = () => {
     const TakeToPassword = () => {
         navigate('/login/password');
     }
->>>>>>> ashehal
   return (
     <Form>
         <Form.Group className='mb-3' controlId='login-email'>
@@ -22,11 +17,7 @@ const Email = () => {
             <Form.Control type='email'></Form.Control>
         </Form.Group>
         <Container className='d-grid p-0'>
-<<<<<<< HEAD
-            <Button type='submit' variant='primary' size='sm'></Button>
-=======
             <Button type='submit' variant='primary' size='sm' onClick={TakeToPassword}>Submit</Button>
->>>>>>> ashehal
         </Container>
     </Form>
 
