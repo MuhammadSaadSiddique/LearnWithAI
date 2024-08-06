2d0ddb586aa8a5fae8724eb7453514f3a69f037c
Ashehal Sadik <ashehalsadik@192.168.1.13>
2024-08-06 13:47:04 +0500

    Embellish login page, make a password page, improve it for mobile usage.

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -1,8 +1,15 @@
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
 
 const Email = () => {
+
+    const navigate = useNavigate();
+
+    const TakeToPassword = () => {
+        navigate('/login/password');
+    }
   return (
     <Form>
         <Form.Group className='mb-3' controlId='login-email'>
@@ -10,7 +17,7 @@ const Email = () => {
             <Form.Control type='email'></Form.Control>
         </Form.Group>
         <Container className='d-grid p-0'>
-            <Button type='submit' variant='primary' size='sm'></Button>
+            <Button type='submit' variant='primary' size='sm' onClick={TakeToPassword}>Submit</Button>
         </Container>
     </Form>
 
